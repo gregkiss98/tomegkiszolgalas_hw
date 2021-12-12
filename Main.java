84f65735f86bbfc675ab6217814af702f38e4363
gregkiss98 <kissge@gain.uni-sopron.hu>
2021-12-12 12:12:56 +0100

    new states and a figure

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ class Main{
     String input = sc.next();
     
     for (int i = 0; i < input.length(); i++) {
-        //haz, hazat, haznak
+        //haz, hazat, haznak, hazzal, hazert, hazza, hazig
         switch(state){
             case 0: switch (input.charAt(i)){
                 case 'h': state = 1; break;
@@ -16,28 +16,59 @@ class Main{
             } break;
             case 1: switch (input.charAt(i)){
                 case 'a': state = 2; break;
+                case 'h': state = 1; break;
                 default: state = 0; break;
             } break;
             case 2: switch (input.charAt(i)){
                 case 'z': state = 3; break;
+                case 't': state = 4; break;
+                case 'k': state = 6; break;
+                case 'l': state = 7; break;
+                case 'h': state = 1; break;
                 default: state = 0; break;
             } break;
             case 3: switch (input.charAt(i)){
-                case 'a': state = 5; break;
-                case 'n': state = 4; break;
+                case 'z': state = 3; break;
+                case 'a': state = 2; break;
+                case 'n': state = 5; break;
+                case 'e': state = 8; break;
+                case 'i': state = 10; break;
+                case 'h': state = 1; break;
                 default: state = 0; break;
             } break;
-            case 4: switch (input.charAt(i)){
-                case 'a': state = 5; break;
-                default: state = 0; break;
+            case 4: switch (input.charAt(i)){ //elfogadó allapot
+                default: state = 4; break;
             } break;
             case 5: switch (input.charAt(i)){
-                case 'k': state = 7; break;
-                case 't': state = 6; break;
+                case 'a': state = 2; break;
                 default: state = 0; break;
+            } break;
+            case 6: switch (input.charAt(i)){ //elfogadó allapot
+                default: state = 6; break;
+            } break;
+            case 7: switch (input.charAt(i)){ //elfogadó allapot
+                default: state = 7; break;
+            } break;
+            case 8: switch (input.charAt(i)){
+                case 'r': state = 9; break;
+                case 'h': state = 1; break;
+                default: state = 0; break;
+            } break;
+            case 9: switch (input.charAt(i)){
+                case 't': state = 4; break;
+                case 'h': state = 1; break;
+                default: state = 0; break;
+            } break;
+            case 10: switch (input.charAt(i)){
+                case 'g': state = 11; break;
+                case 'h': state = 1; break;
+                default: state = 0; break;
+            } break;
+            case 11: switch (input.charAt(i)){ //elfogadó allapot
+                default: state = 11; break;
             } break;
         }
     }
-    System.out.println(state == 3 || state == 6 || state == 7 ? "Elfogadva":"Nem elfogadva");
+    System.out.println(state == 3 || state == 4 || state == 6 || state == 7 || state == 11 ? "Elfogadva":"Nem elfogadva");
     }
 }
